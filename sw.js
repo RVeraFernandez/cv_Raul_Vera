774cddcf9d35c070cf75a4f3fab46b82f0782420
RVeraFernandez <raulverafernandez3@gmail.com>
2023-03-04 21:15:32 -0600

    Cambio en sw.js parte 3 parte 3

--- sw.js
+++ sw.js
@@ -24,50 +24,52 @@ var urlsToCache = [
     "./img/1024.png",
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache).then(() => {
-          self.skipWaiting();
-        });
-      })
-      .catch(err => console.log('No se ha registrado el cache',err))
-  );
-});
-
-//Event activate
-self.addEventListener("activate", (e) => {
-  const cacheWhiteList = [CACHE_NAME];
+self.addEventListener('install', e=>{
 
   e.waitUntil(
-    caches.keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheNames) => {
-            if (cacheWhiteList.indexOf(cacheNames) == -1) {
-              return cache.delete(cacheNames);
-            }
-          })
-        );
+      caches.open(CACHE_NAME)
+      .then(cache => {
+          return cache.addAll(urlsToCache)
+                      .then(() =>{
+                          self.skipWaiting(); 
+                      })
       })
-      .then(() => {
-        self.clients.claim();
-      })
-  );
+      .catch(err=>console.log('No se ha registrado el cache', err))
+);
 });
 
+self.addEventListener('activate',e => {
+  const cacheWhitelist = [CACHE_NAME];
 
-//Event fetch
-self.addEventListener("fetch", (e) => {
+
+  e.waitUntil(
+      caches.keys()
+              .then(cacheNames=>{
+              return Promise.all(
+                  cacheNames.map(cacheName =>{
+                      if(cacheWhitelist.indexOf(cacheName)== -1)
+                      {
+
+                          return cache.delete(cacheName);
+                      }
+                  })
+              );
+             })
+             .then(()=> {
+              self.clients.claim(); 
+             })
+  );
+
+});
+
+self.addEventListener('fetch',e => {
   e.respondWith(
-    caches.match(e.request)
-    .then(res => {
-      if (res) {
-        return res;
-      }
-      return fetch(e.request);
-    })
+      caches.match(e.request)
+              .then (res => {
+                  if(res){
+                      return res;
+                  }
+                  return fetch(e.request); 
+              })
   );
 });
