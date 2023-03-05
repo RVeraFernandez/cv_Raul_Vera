6592e71c5b6695176a4a5189052a8725ef368c66
RVeraFernandez <raulverafernandez3@gmail.com>
2023-03-04 20:18:29 -0600

    Movimiento del archivo main y correxión de sw

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator){
     console.log("Puedes usar el service worker")
 
-    navigator.serviceWorker.register('../sw.js')
+    navigator.serviceWorker.register('./sw.js')
     .then(res => console.log('SW cargado correctamente', res))
     .catch(err => console.log('Service worker no se ha podido registrar', err));
 }
