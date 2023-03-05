ce437043cd91f8e8ecbb4fc68c4e4072e4b35173
RVeraFernandez <raulverafernandez3@gmail.com>
2023-03-04 19:42:04 -0600

    Correxión de sw.js 2

--- sw.js
+++ sw.js
@@ -1,32 +1,32 @@
 const CACHE_NAME = "v1_cache1_RFVF_PWA";
 
 var urlsToCache = [
-    "/",
+    "./",
     "./index.html",
-    "css/styles.css",
+    "./css/styles.css",
     "./js/main.js",
-    "img/Square44x44Logo.targetsize-32.png",
-    "img/Square44x44Logo.targetsize-48.png",
-    "img/Square44x44Logo.targetsize-64.png",
-    "img/Square44x44Logo.targetsize-72.png",
-    "img/Square44x44Logo.targetsize-96.png",
-    "img/Square44x44Logo.targetsize-256.png",
-    "img/android-launchericon-512-512.png",
-    "img/android-launchericon-192-192.png",
-    "img/android-launchericon-144-144.png",
-    "img/android-launchericon-96-96.png",
-    "img/android-launchericon-72-72.png",
-    "img/android-launchericon-48-48.png",
-    "img/40.png",
-    "img/80.png",
-    "img/256.png",
-    "img/512.png",
-    "img/1024.png",
+    "./img/Square44x44Logo.targetsize-32.png",
+    "./img/Square44x44Logo.targetsize-48.png",
+    "./img/Square44x44Logo.targetsize-64.png",
+    "./img/Square44x44Logo.targetsize-72.png",
+    "./img/Square44x44Logo.targetsize-96.png",
+    "./img/Square44x44Logo.targetsize-256.png",
+    "./img/android-launchericon-512-512.png",
+    "./img/android-launchericon-192-192.png",
+    "./img/android-launchericon-144-144.png",
+    "./img/android-launchericon-96-96.png",
+    "./img/android-launchericon-72-72.png",
+    "./img/android-launchericon-48-48.png",
+    "./img/40.png",
+    "./img/80.png",
+    "./img/256.png",
+    "./img/512.png",
+    "./img/1024.png",
 ];
 
-self.addEventListener('install', event =>{
+self.addEventListener('install', (event) =>{
     event.waitUntil(
-        caches.open('mi-cache').then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(urlsToCache).then(()=>{
                 self.skipWaiting()
             })
@@ -35,7 +35,7 @@ self.addEventListener('install', event =>{
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response)=> {
             return response || fetch(event.request);
