ec0ebd11d6fb91cab136513723d8961e27f37f93
RVeraFernandez <raulverafernandez3@gmail.com>
2023-03-04 22:26:47 -0600

    Cambiar config de sw parte 3 parte 1

--- sw.js
+++ sw.js
@@ -25,51 +25,38 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', e => {
-
-  e.waitUntil(
-      caches.open(CACHE_NAME)
-      .then(cache => {
-          return cache.addAll(urlsToCache)
-                      .then(() => {
-                          self.skipWaiting(); 
-                      })
-      })
-      .catch(err => console.log('No se ha registrado el cache', err))
-);
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => {
+    return cache.addAll(urlsToCache)
+    .then(() => {
+      self.skipWaiting(); 
+    })
+  })
+  .catch(err => console.log('No se ha registrado el cache', err))
+  );
 });
 
 self.addEventListener('activate',e => {
   const cacheWhitelist = [CACHE_NAME];
-
-
-  e.waitUntil(
-      caches.keys()
-              .then(cacheNames => {
-              return Promise.all(
-                  cacheNames.map(cacheName =>{
-                      if(cacheWhitelist.indexOf(cacheName)== -1)
-                      {
-
-                          return cache.delete(cacheName);
-                      }
-                  })
-              );
-             })
-             .then(()=> {
-              self.clients.claim(); 
-             })
-  );
-
+  e.waitUntil(caches.keys().then(cacheNames => {
+    return Promise.all(
+      cacheNames.map(cacheName =>{
+        if(cacheWhitelist.indexOf(cacheName)== -1){
+          return cache.delete(cacheName);
+        }
+      })
+    );
+  }).then(()=> {
+    self.clients.claim(); 
+  }));
 });
 
 self.addEventListener('fetch',e => {
   e.respondWith(
-      caches.match(e.request)
-              .then (res => {
-                  if(res){
-                      return res;
-                  }
-                  return fetch(e.request); 
-              })
+    caches.match(e.request).then (res => {
+      if(res){
+        return res;
+      }
+      return fetch(e.request);
+    })
   );
 });
