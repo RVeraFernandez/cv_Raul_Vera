f4f7cd57cb4c338f008db4a30cebd0dc4ddc665a
RVeraFernandez <raulverafernandez3@gmail.com>
2023-03-04 19:52:48 -0600

    Código corregido en sw.js

--- sw.js
+++ sw.js
@@ -26,27 +26,30 @@ var urlsToCache = [
 
 self.addEventListener('install', (event) =>{
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(urlsToCache).then(()=>{
-                self.skipWaiting()
-            })
-            .catch(err=>console.log('No se ha registrado el cache',err))
-        })
+                self.skipWaiting();
+            });
+        }).catch(err=>console.log('No se ha registrado el cache',err))
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response)=> {
-            return response || fetch(event.request);
+        caches.match(event.request)
+        .then((response)=> {
+            if(response){
+                return response
+            }
+            return fetch(event.request);
         })
     );
 });
 
-self.addEventListener('activate',event => {
+self.addEventListener('activate',(event) => {
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
-        caches.keys().then(cacheNames=>{
+        caches.keys().then((cacheNames)=>{
             return Promise.all(
                 cacheNames.map(cacheNames => {
                     if(cacheWhitelist.indexOf(cacheNames)== -1){
